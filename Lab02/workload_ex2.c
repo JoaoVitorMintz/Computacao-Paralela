61c4ae575b81f357e7a0cb2f1d1a2022dcc07eff
JoaoVitorMintz <joaomintzz@gmail.com>
2026-02-28 15:34:32 -0300

    [UPDATE] - Added Parallel exercises from Lab02, answered the questions and tested it

---
+++ Lab02/workload_ex2.c
@@ -0,0 +1,66 @@
+// Medindo desempenho Paralelo
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define VECTOR_SIZE 200000000
+#define NUM_PROCESSES 4
+#define TEMPO_SEQUENCIAL 4.502869
+
+ // Funcao que simula uma carga de trabalho pesada
+void heavy_work (double * vector , int start , int end) {
+	for (int i = start ; i < end ; ++i) {
+		vector [i] = sin( vector [i]) * cos( vector [i]) + sqrt ( vector [i]);
+	}
+}
+
+int main () {
+    double * vector = ( double *) malloc ( VECTOR_SIZE * sizeof ( double ));
+    for(int i = 0; i < VECTOR_SIZE ; i++) vector [i] = ( double )i;
+
+    struct timespec start , end;
+    int chunk_size = VECTOR_SIZE / NUM_PROCESSES;
+    pid_t pid;
+
+    clock_gettime (CLOCK_MONOTONIC , & start);
+
+    for (int i = 0; i < NUM_PROCESSES-1; i++) {
+        pid = fork();
+        if (pid == 0) {
+            int inicio = i * chunk_size;
+            int fim;
+
+            // Averiguar para ir até o final corretamente
+            if (i == NUM_PROCESSES -1) {
+                fim = VECTOR_SIZE;
+            } else {
+                fim = (i + 1) * chunk_size;
+            }
+
+            heavy_work (vector , inicio, fim);
+            exit(0);
+        } else {
+            for (int i = 0; i < NUM_PROCESSES; i++) {
+                wait(NULL);
+            }
+        }
+    }
+    
+    clock_gettime ( CLOCK_MONOTONIC , &end);
+
+    double time_spent = (end. tv_sec - start . tv_sec ) + (end. tv_nsec - start . tv_nsec ) / 1e9;
+
+    printf("Versão sequencial executou em %f segundos\n", TEMPO_SEQUENCIAL);
+    printf("Versao paralelizada com fork executou em %f segundos\n", time_spent );
+    printf("SPEEDUP COM O FORK: %f\n", TEMPO_SEQUENCIAL/time_spent);
+
+    // Para verificar se o calculo foi feito , descomente a linha abaixo :
+    // printf (" Resultado de verificacao : vector [10] = %f\n", vector [10]) ;
+
+    free (vector);
+    return 0;
+}
